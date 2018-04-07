458d036cf7fbd99ac2ac7bb8f516b65e0e697bb6
Derocox <derocox@gmail.com>
2018-04-07 09:42:01 -0500

    Se corrie prueba unitaria

--- spec/DiccionarioAhorcado_spec.rb
+++ spec/DiccionarioAhorcado_spec.rb
@@ -3,6 +3,11 @@ require './lib/DiccionarioAhorcado.rb'
 describe DiccionarioAhorcado do
 	it "Tener arreglo de palabras" do
 		arrayPalabra = DiccionarioAhorcado.new
-		expect(arrayPalabra.obtenerArreglo).to eq "palabra1,palabra2,palabra3"
-	end	
+		expect(arrayPalabra.obtenerArreglo).to eq "palabra1, palabra2, palabra3"
+	end
+
+	it "Seleccionar palabra aleatoriamente" do
+		arrayPalabra = DiccionarioAhorcado.new
+		expect(arrayPalabra.obtenerPalabra).to eq "palabra3"
+	end		
 end
