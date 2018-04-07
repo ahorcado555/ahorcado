34e8b83242443bf0355d20e2155c2fa3b3234cca
Derocox <derocox@gmail.com>
2018-04-07 09:58:01 -0500

    Se agrega nueva prueba unitaria

--- lib/DiccionarioAhorcado.rb
+++ lib/DiccionarioAhorcado.rb
@@ -1,8 +1,22 @@
 class DiccionarioAhorcado
 	def initialize
-		@arregloPalabras = "palabra1, palabra2, palabra3"
+		@arregloPalabras = ["palabra1", "palabra2", "palabra3"]
+		@numeroAleatorio = 0
 	end
 	def obtenerArreglo
-		@arregloPalabras
+		strPalabra = ""
+		@arregloPalabras.each do |palabra|
+			strPalabra += palabra + ", "
+		end
+		strPalabra =  strPalabra[0..-3]
+		return strPalabra
 	end
+
+	def obtenerPalabra
+		@arregloPalabras[@numeroAleatorio]
+	end
+
+	def definirNumeroAleatorio numero = rand(3)
+		@numeroAleatorio = numero
+	end 
 end
--- spec/DiccionarioAhorcado_spec.rb
+++ spec/DiccionarioAhorcado_spec.rb
@@ -4,5 +4,11 @@ describe DiccionarioAhorcado do
 	it "Tener arreglo de palabras" do
 		arrayPalabra = DiccionarioAhorcado.new
 		expect(arrayPalabra.obtenerArreglo).to eq "palabra1, palabra2, palabra3"
+	end
+
+	it "Seleccionar palabra aleatoriamente" do
+		arrayPalabra = DiccionarioAhorcado.new
+		arrayPalabra.definirNumeroAleatorio 2
+		expect(arrayPalabra.obtenerPalabra).to eq "palabra3"
 	end		
 end
